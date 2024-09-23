883d76869efd5e3b47b89a31e67cbcd1f04deecc
Aaron-igt <145329946+Aaron-igt@users.noreply.github.com>
2024-09-24 00:45:49 +0300

    Add files via upload

---
+++ madlibs_project.py
@@ -0,0 +1,11 @@
+# creating madlibs using string concatenation
+# writing a program to allow a user to input the information needed in the blank spaces since
+# it's a madlib
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("famous person: ")
+madlib = f"Computer programming is so {adj}! it makes me so excited all the time because \
+I love to {verb1}, Stay hydrated and {verb2} like you are {famous_person}"
+
+print(madlib)
