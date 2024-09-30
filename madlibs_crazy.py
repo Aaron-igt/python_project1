a04233ab428ad5aa7b5d97b8942c9f4f20e56a4a
Aaron-igt <145329946+Aaron-igt@users.noreply.github.com>
2024-09-30 11:55:08 +0300

    Add files via upload

---
+++ madlibs_crazy.py
@@ -0,0 +1,51 @@
+# madlibs project sbout a story called a crazy adventure.
+
+# create variables asking the user for input acting as blank spaces to be 
+# filled in the madlib game.
+adjective1 = input("adjective1: ")
+name1 = input("name1: ")
+place1 = input("place1: ")
+noun1 = input("noun1: ")
+vehicle1 = input("vehicle1: ")
+adjective2 = input("adjective2: ")
+animal1 = input("animal1: ")
+noun2 = input("noun2: ")
+adjective3 = input("adjective3: ")
+pluralNoun1 = input("plural noun1: ")
+adjective4 = input("adjective4: ")
+adjective5 = input("adjective5: ")
+pluralNoun2 = input("plural noun2: ")
+typeOfMusic = input("type of music: ")
+adjective6 = input("adjective6: ")
+noun3 = input("noun3: ")
+food1 = input("food1: ")
+adjective7 = input("adjective7: ")
+adjective8 = input("adjective8: ")
+adjective9 = input("adjective9: ")
+place2 = input("place2: ")
+pluralNoun4 = input("plural noun4: ")
+adjective11 = input("adjective11: ")
+
+
+# creating main variable to hold the madlib story.
+crazyAdventure =f"""
+                                THE CRAZY ADVENTURE
+    It was a {adjective1} morning when {name1} woke up and realized they were late for the trip to
+{place1}. They quickly jumped out of bed, grabbed their favourite {noun1}, and ran to the {vehicle1} 
+parked outside. As they zoomed down the street, they accidently knocked over a {adjective2} {animal1}
+that was carrying a huge {noun2} on it's back.
+    \"Sorry!\" {name1} shouted, feeling {adjective3}. When they finally arrived at {place1}, they realized 
+they had forgotten to bring their {pluralNoun1}! \"How could I forget my {pluralNoun1}?\" they thought.
+    Determined not to let that ruin their adventure, {name1} decided to explore the {adjective4} forest 
+nearby. As they wandered through the trees, they heard a {adjective5} noise. it sounded like a group of
+{pluralNoun2} singing {typeOfMusic}! Curious, they followed the sound and stumbled upon a secret gathering 
+of {adjective6} creatures dancing around a giant {noun3}.
+    One of the creatures approached {name1} and offered them a {food1}. It tasted {adjective7}, and soon, {name1}
+felt increndibly {adjective8}. Suddenly, the creatures invited them to ride a {noun3} that could fly!
+    Excited, {name1}hopped on the doared into the sky, flying over mountains, rivers, and even a {adjective9} {place2}.
+They couldn't believe their eyes when they saw a {adjective9} dragon chasing after a {pluralNoun4}.
+    \"That was the most {adjective11} day of my life!\" {name1} said, collapsing into bed with a smile on their face.
+"""
+
+# execute the madlib game
+print(crazyAdventure)
